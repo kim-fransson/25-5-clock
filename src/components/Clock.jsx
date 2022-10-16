96c57e479c5697798d0ad4b52f617319693da211
Kim Fransson <kim.fransson@sigmatechnology.se>
2022-10-16 16:41:04 +0200

    counters implemented

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,41 +1,71 @@
+import { useReducer } from "react";
+import Counter from "./Counter";
+
+const COUNTER_ACTIONS = {
+  BREAK_INCREMENT: "break-increment",
+  BREAK_DECREMENT: "break-decrement",
+  SESSION_INCREMENT: "session-increment",
+  SESSION_DECREMENT: "session-decrement",
+};
+
+const CLOCK_ACTIONS = {
+  RESET: "reset",
+};
+
+const DEFAULT_STATES = {
+  breakLength: 5,
+  sessionLength: 25,
+};
+
+const counterReducer = (state, { type }) => {
+  switch (type) {
+    case COUNTER_ACTIONS.BREAK_INCREMENT:
+      return { ...state, breakLength: state.breakLength++ };
+    case COUNTER_ACTIONS.BREAK_DECREMENT:
+      return { ...state, breakLength: state.breakLength-- };
+    case COUNTER_ACTIONS.SESSION_INCREMENT:
+      return { ...state, sessionLength: state.sessionLength++ };
+    case COUNTER_ACTIONS.SESSION_DECREMENT:
+      return { ...state, sessionLength: state.sessionLength-- };
+    case CLOCK_ACTIONS.RESET:
+      return DEFAULT_STATES;
+  }
+};
+
 function Clock() {
+  const [{ breakLength, sessionLength }, dispatch] = useReducer(
+    counterReducer,
+    {
+      breakLength: 5,
+      sessionLength: 25,
+    }
+  );
+
   return (
     <div className="border-4 gap-y-4 rounded-lg shadow-lg bg-slate-700 border-gray-900 py-4 mx-auto max-w-[500px] grid-cols-4 grid text-white">
       <h1 className="col-span-full text-center text-4xl">
         25 + 5 <span className="font-semibold text-red-600">Clock</span>
       </h1>
-      <div className="col-span-2 flex gap-1 flex-col items-center">
-        <p id="break-label" className="text-xl">
-          Break Length
-        </p>
-        <div className="flex gap-2 items-center">
-          <button id="break-decrement" className="text-4xl">
-            ⬇️
-          </button>
-          <span id="break-length" className="text-3xl">
-            5
-          </span>
-          <button id="break-increment" className="text-4xl">
-            ⬆️
-          </button>
-        </div>
-      </div>
-      <div className="col-span-2 flex flex-col items-center gap-1">
-        <p id="session-label" className="text-xl">
-          Session Length
-        </p>
-        <div className="flex gap-2 items-center">
-          <button id="session-decrement" className="text-4xl">
-            ⬇️
-          </button>
-          <span id="session-length" className="text-3xl">
-            25
-          </span>
-          <button id="session-increment" className="text-4xl">
-            ⬆️
-          </button>
-        </div>
-      </div>
+      <Counter
+        id="break"
+        value={breakLength}
+        handleDecrement={() =>
+          dispatch({ type: COUNTER_ACTIONS.BREAK_DECREMENT })
+        }
+        handleIncrement={() =>
+          dispatch({ type: COUNTER_ACTIONS.BREAK_INCREMENT })
+        }
+      />
+      <Counter
+        id="session"
+        value={sessionLength}
+        handleDecrement={() =>
+          dispatch({ type: COUNTER_ACTIONS.SESSION_DECREMENT })
+        }
+        handleIncrement={() =>
+          dispatch({ type: COUNTER_ACTIONS.SESSION_INCREMENT })
+        }
+      />
       <div className="col-span-full flex flex-col items-center border-4 rounded-full w-40 h-40 justify-center mx-auto">
         <p id="timer-label" className="text-3xl">
           Session
@@ -48,7 +78,11 @@ function Clock() {
         <button id="start_stop" className="text-4xl">
           ⏯
         </button>
-        <button id="reset" className="text-4xl">
+        <button
+          id="reset"
+          className="text-4xl"
+          onClick={() => dispatch({ type: CLOCK_ACTIONS.RESET })}
+        >
           ⟳
         </button>
       </div>
---
+++ src/components/Counter.jsx
@@ -0,0 +1,32 @@
+function Counter({ id, value, handleDecrement, handleIncrement }) {
+  return (
+    <div className="col-span-2 flex gap-1 flex-col items-center">
+      <p id={`${id}-label`} className="text-xl capitalize">
+        {id} length
+      </p>
+      <div className="flex gap-2 place-items-center">
+        <button
+          id={`${id}-decrement`}
+          className="text-4xl disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={handleDecrement}
+          disabled={value === 0}
+        >
+          ⬇️
+        </button>
+        <span id={`${id}-length`} className="text-3xl flex-grow-0">
+          {value}
+        </span>
+        <button
+          id={`${id}-increment`}
+          className="text-4xl disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={handleIncrement}
+          disabled={value === 59}
+        >
+          ⬆️
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
